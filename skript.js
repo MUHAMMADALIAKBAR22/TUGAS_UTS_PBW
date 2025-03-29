8d3ef8e3d81d4d923867cc919c569f749ae2e5f7
MUHAMMADALIAKBAR22 <muhammadaliakbar248@gmail.com>
2025-03-29 16:39:47 +0700

    Update UTS

--- skript.js
+++ skript.js
@@ -39,4 +39,23 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+    const backToTopButton = document.getElementById("back-to-top");
+    
+    // When the user scrolls down 300px from the top of the document, show the button
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+    
+    // When the user clicks on the button, scroll to the top of the document
+    backToTopButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
